24e4a6b2e199225a62d0a4fedde40631413b60b1
meanwhile131 <51206659+meanwhile131@users.noreply.github.com>
2025-12-05 17:41:36 +0400

    Use telegram.ext and add /start

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import os
-import asyncio
-import telegram
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.WARN
+)
 token = os.getenv("BOT_TOKEN")
 if not token:
-    raise ValueError("Set BOT_TOKEN env variable")
+    logging.critical("BOT_TOKEN environment variable not found")
+    exit(1)
 
-async def main():
-    bot = telegram.Bot(token)
-    async with bot:
-        print(await bot.get_me())
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="""Команды:
+/new - создать голосование
+""")
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    application = ApplicationBuilder().token(token).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
